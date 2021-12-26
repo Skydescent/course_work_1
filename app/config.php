558d311c947039e56255c2431145f435dbc69d13
Kirill Kalashnikov <kirill310587@mail.ru>
2021-12-26 23:41:11 +0700

    Dockerized project

---
+++ app/config.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Директория дла загрузки изображений
+ */
+define('IMG_PATH', 'img/products/');
+
+/**
+ * DSN для связи с базой данных
+ */
+define('DB_DSN', 'mysql:host=db;dbname=fashion;charset=utf8;'); //'mysql:host=app;dbname=fashion;charset=utf8;'
+
+/**
+ * Логин для подключения к базе данных
+ */
+define('DB_LOGIN', 'root');
+
+/**
+ * Пароль для подключения к базе данных
+ */
+define('DB_PASSWORD', 'your_mysql_root_password');
+
+/**
+ * Формат даты для добавления полей в таблицы БД
+ */
+define('DB_DATE_FORMAT', 'Y-m-d');
+
+/**
+ * Стоимость доставки товара для клиента при выборе курьерской доставки
+ */
+define('DELIVERY_COST', 280);
+
